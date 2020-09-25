fc81eab65a9fa54aa64ccb3b4b33ac450ef2df8b
Z00fers <david.thilman@elev.ga.ntig.se>
2020-09-25 10:52:36 +0200

    ep 7,8

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function show($post)
+    {
+        $posts = [
+            'oof' => 'hmm',
+            'derp' => 'herp'
+        ];
+
+        if (!array_key_exists($post, $posts))
+        {
+            abort(404, 'no');
+        }
+
+        return view('post', [
+            'post' => $posts[$post]
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('posts/{post}', [PostController::class, 'show']);
+// Route::get('posts/{post}', 'app\Http\Controllers\PostController@show');

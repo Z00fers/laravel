0aa5399e8cec7827dd3cb299eed54b41b6d47ea1
Z00fers <david.thilman@elev.ga.ntig.se>
2020-09-28 09:17:39 +0200

    ep 10

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,18 +3,15 @@
 namespace App\Http\Controllers;
 
 use DB;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
-        $post = DB::table('posts')->where('slug', $slug)->first();
-
-        // dd($post);
-
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }

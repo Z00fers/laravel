87c0e656a9de09881afdb8ea611ccb4fd89ad960
Z00fers <david.thilman@elev.ga.ntig.se>
2020-10-06 14:26:23 +0200

    ep 29

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,15 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Article extends Model
+class Project extends Model
 {
     use HasFactory;
-    protected $guarded = [];
-
-    public function path()
-    {
-        return route('article.show', $this);
-    }
 
     public function user()
     {
